00b19a129d5f35b4e2f0a87837c2cfad07faede1
Felipe-Meneguzz1 <lipemeneguzzi@gmail.com>
2025-01-16 20:37:00 -0300

    Product

---
+++ src/main/java/com/workshop/workshop/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.workshop.workshop.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.workshop.workshop.entities.Order;
+import com.workshop.workshop.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
