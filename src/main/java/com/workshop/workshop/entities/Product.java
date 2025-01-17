beaa7c1dd9a402d6a2eabf70c331827b757b020d
Felipe-Meneguzz1 <lipemeneguzzi@gmail.com>
2025-01-17 20:10:25 -0300

    Payment one-to-one association

--- src/main/java/com/workshop/workshop/entities/Product.java
+++ src/main/java/com/workshop/workshop/entities/Product.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -89,15 +91,16 @@ public class Product implements Serializable {
 	public Set<Category> getCategories() {
 		return categories;
 	}
-	
-	public Set<Order> getOrders(){
+
+	@JsonIgnore
+	public Set<Order> getOrders() {
 		Set<Order> set = new HashSet<>();
-		for(OrderItem x : items) {
+		for (OrderItem x : items) {
 			set.add(x.getOrder());
 		}
 		return set;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -105,7 +108,7 @@ public class Product implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
